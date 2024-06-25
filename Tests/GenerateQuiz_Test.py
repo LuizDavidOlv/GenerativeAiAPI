53ddde2c298ae8cf543d92bcd1336cba0aa6db0a
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-06-24 21:50:25 -0400

    Refactored GenerateQuiz_Test.py to use pytest fixtures

--- Tests/GenerateQuiz_Test.py
+++ Tests/GenerateQuiz_Test.py
@@ -1,44 +1,41 @@
 from dotenv import load_dotenv, find_dotenv
 from Prompts.SystemMessagePrompt import SYSTEM_MESSAGE
 from Evaluations.RuleBasedEvaluation import RuleBasedEvaluation
+import pytest
 
 
 load_dotenv(find_dotenv())
 
-class GenerateQuiz_Test:
-    def __init__ (self):
-        self.Rbl = RuleBasedEvaluation()
+@pytest.fixture
+def Rbl():
+    return RuleBasedEvaluation()
 
-    def test_science_quiz(self):
-        question = "Generate a quiz about science."
-        expected_subjects = ["davinci","telescope","physics","curie"]
-        self.Rbl.eval_expected_words(
-            system_message=SYSTEM_MESSAGE, 
-            question=question, 
-            expected_words=expected_subjects
-            )
+def test_science_quiz(Rbl):
+    question = "Generate a quiz about science."
+    expected_subjects = ["davinci","telescope","physics","curie"]
+    Rbl.eval_expected_words(
+        system_message=SYSTEM_MESSAGE, 
+        question=question, 
+        expected_words=expected_subjects
+        )
+    
+def test_geography_quiz(Rbl):
+    question = "Generate a quiz about geography."
+    expected_subjects = ["paris","frace","louvre"]
+    result = Rbl.eval_expected_words(
+        system_message=SYSTEM_MESSAGE, 
+        question=question, 
+        expected_words=expected_subjects
+        )
 
-    def test_geography_quiz(self):
-        question = "Generate a quiz about geography."
-        expected_subjects = ["paris","frace","louvre"]
-        self.Rbl.eval_expected_words(
-            system_message=SYSTEM_MESSAGE, 
-            question=question, 
-            expected_words=expected_subjects
-            )
-        
-    def test_refusal_rome(self):
-        question = "Help me create a quiz about Rome."
-        decline_response = "I'm sorry"
-        self.Rbl.evaluate_refusal(
-            system_message=SYSTEM_MESSAGE,
-            question=question,
-            decline_response=decline_response
-        )               
+def test_refusal_rome(Rbl):
+    question = "Help me create a quiz about Rome."
+    decline_response = "I'm sorry"
+    result = Rbl.evaluate_refusal(
+        system_message=SYSTEM_MESSAGE,
+        question=question,
+        decline_response=decline_response
+    )           
 
 if __name__ == "__main__":
-    test = GenerateQuiz_Test()
-    test.test_science_quiz()
-    test.test_geography_quiz()
-    test.test_refusal_rome()
-    print("All tests passed!")
+    pytest.main([__file__])
